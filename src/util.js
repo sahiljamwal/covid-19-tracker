07078148bd542d070b09b359aa4abdc8ecc077d8
Sahil Jamwal <34044226+sahiljamwal@users.noreply.github.com>
2020-12-24 15:57:48 +0530

    Update util.js

--- src/util.js
+++ src/util.js
@@ -8,27 +8,6 @@ export const sortData = (data) => {
   return sortedData;
 };
 
-// to chose color for circles
-// const casesTypeColors = {
-//   cases: {
-//     hex: "#CC1034",
-//     rgb: "rgb(204, 16, 52)",
-//     half_op: "rgba(204, 16, 52, 0.5)",
-//     multiplier: 800,
-//   },
-//   recovered: {
-//     hex: "#7dd71d",
-//     rgb: "rgb(125, 215, 29)",
-//     half_op: "rgba(125, 215, 29, 0.5)",
-//     multiplier: 1200,
-//   },
-//   deaths: {
-//     hex: "#fb4443",
-//     rgb: "rgb(251, 68, 67)",
-//     half_op: "rgba(251, 68, 67, 0.5)",
-//     multiplier: 2000,
-//   },
-// };
 const casesTypeColors = {
   cases: {
     multiplier: 800,
@@ -46,21 +25,6 @@ const casesTypeColors = {
 // to show circles on map according to size of the
 export const showDataOnMap = (data, casesType = "cases") =>
   data.map((country) => (
-    // <Circle
-    //   // center={[country.countryInfo.lat, country.countryInfo.long]}
-    //   // color={casesTypeColors[casesType].hex}
-    //   // fillColor={casesTypeColors[casesType].hex}
-    //   // fillOpacity={0.4}
-    //   // radius={
-    //   //   Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-    //   // }
-    //   center={[country.countryInfo.lat, country.countryInfo.long]}
-    //   fillOpacity={0.4}
-    //   pathOptions={casesTypeColors[casesType].option}
-    //   radius={
-    //     Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-    //   }
-    // >
     <Circle
       center={[country.countryInfo.lat, country.countryInfo.long]}
       fillOpacity={0.4}
